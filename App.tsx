5257e858bc9b615980b7b7a6b0292b4168f11f48
Reece437 <munningsreece@gmail.com>
2022-08-31 10:49:49 +0100

    Added drawer navigation for settings

--- App.tsx
+++ App.tsx
@@ -8,43 +8,66 @@ import WelcomeScreen from "./screens/Welcome_Screen.tsx";
 import { TailwindProvider } from "tailwindcss-react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { auth } from "./firebase";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Settings from './screens/Settings';
+
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HomeScreen = () => (
+  <Drawer.Navigator screenOptions={{
+  	drawerStyle: {
+  		backgroundColor: '#121212'
+  	},
+  	swipeEdgeWidth: 60
+  }}>
+    <Drawer.Screen
+      component={Home}
+      name="Footdle"
+      options={{ headerShown: false }}
+    />
+    <Drawer.Screen
+      component={Settings}
+      name="Settings"
+      options={{ headerShown: false }}
+    />
+  </Drawer.Navigator>
+);
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [signedIn, setSignedIn] = useState()
-   useEffect(() => {
+  const [signedIn, setSignedIn] = useState();
+  useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        //AsyncStorage.setItem('signedIn', JSON.stringify(true));
         setSignedIn(true);
       } else setSignedIn(false);
     });
     return unsubscribe;
   }, []);
-  
+
   useEffect(() => {
-  	const setScreen = async() => {
-  		if (signedIn !== undefined) {
-  			await SplashScreen.hideAsync();
-  		}
-  	}
-  	setScreen();
-  }, [signedIn])
-  
+    const setScreen = async () => {
+      if (signedIn !== undefined) {
+        await SplashScreen.hideAsync();
+      }
+    };
+    setScreen();
+  }, [signedIn]);
+
   if (signedIn == undefined) {
-  	return null;
+    return null;
   }
-  
+
   return (
     <NavigationContainer>
       <TailwindProvider>
         <Stack.Navigator initialRouteName={signedIn ? "Home" : "WelcomeScreen"}>
           <Stack.Screen
-            component={Home}
-            name="Home"
+            component={HomeScreen}
+            name="HomeScreen"
             options={{ headerShown: false }}
           />
           <Stack.Screen
